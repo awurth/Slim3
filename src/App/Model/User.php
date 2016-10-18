53b3664c83f8461f959464a58a25522a469d6329
Alexis Wurth <alexis.wurth57@gmail.com>
2016-10-18 16:32:56 +0200

    Added user table schema in its eloquent model

--- src/App/Model/User.php
+++ src/App/Model/User.php
@@ -3,10 +3,23 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Capsule\Manager;
+use Illuminate\Database\Schema\Blueprint;
 
 class User extends Model
 {
     protected $table = 'user';
 
     protected $primaryKey = 'id';
+
+    public static function createTable()
+    {
+        Manager::schema()->create('user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
 }
