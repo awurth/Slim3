581398a264215a3a4bee2270fbead6a90ab12392
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-04 21:58:05 +0100

    Refactoring

--- src/App/Controller/Controller.php
+++ src/App/Controller/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Http\Message\ResponseInterface as Response;
 use Interop\Container\ContainerInterface;
 use Slim\Flash\Messages;
 use Slim\Router;
@@ -26,15 +27,22 @@ class Controller
         $this->container = $container;
     }
 
-    public function redirect($route, array $params = array()) {
-        return $this->container->get('response')->withRedirect(
-            $this->pathFor($route, $params)
-        );
+    /**
+     * Redirect to route
+     *
+     * @param Response $response
+     * @param string $route
+     * @param array $params
+     * @return Response
+     */
+    public function redirect(Response $response, $route, array $params = array())
+    {
+        return $response->withRedirect($this->router->pathFor($route, $params));
     }
 
     public function flash($name, $message)
     {
-        $this->container->get('flash')->addMessage($name, $message);
+        $this->flash->addMessage($name, $message);
     }
 
     public function __get($property)
