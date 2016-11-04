d8956c01db1f4daf604c3c188e88b0736e510e81
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-04 22:14:12 +0100

    Added AuthController to container

--- bootstrap/controllers.php
+++ bootstrap/controllers.php
@@ -3,3 +3,7 @@
 $container['AppController'] = function ($container) {
     return new App\Controller\AppController($container);
 };
+
+$container['AuthController'] = function ($container) {
+    return new App\Controller\AuthController($container);
+};
