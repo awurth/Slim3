624e3387845ad83d99148b831f9c932376ae0c70
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-04 22:18:03 +0100

    Added AuthController

---
+++ src/App/Controller/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Respect\Validation\Validator as V;
+
+class AuthController extends Controller
+{
+    public function login(Request $request, Response $response)
+    {
+        if ($request->isPost()) {
+            if (!$this->auth->attempt($request->getParam('username'), $request->getParam('password'))) {
+                $this->flash('danger', 'Wrong username or password.');
+                return $this->redirect($response, 'login');
+            }
+
+            return $this->redirect($response, 'home');
+        }
+
+        return $this->view->render($response, 'Auth/login.twig');
+    }
+}
