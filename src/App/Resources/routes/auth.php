6bc17cb78f25952eb2e945c9dad8485f9b40b3c9
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-05 10:03:24 +0100

    Added route for logout

--- src/App/Resources/routes/auth.php
+++ src/App/Resources/routes/auth.php
@@ -3,4 +3,8 @@
 $app->group('', function () {
     $this->map(['GET', 'POST'], '/login', 'AuthController:login')->setName('login');
     $this->map(['GET', 'POST'], '/register', 'AuthController:register')->setName('register');
-})->add(new \App\Middleware\GuestMiddleware($container));
+})->add(new App\Middleware\GuestMiddleware($container));
+
+$app->group('', function () {
+    $this->get('/logout', 'AuthController:logout')->setName('logout');
+})->add(new App\Middleware\AuthMiddleware($container));
