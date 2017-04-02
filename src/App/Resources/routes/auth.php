eb045ee3fb398c59a9537d11cf79aed12c3e7a41
Alexis Wurth <alexis.wurth57@gmail.com>
2017-04-03 00:50:33 +0200

    Refactoring

--- src/App/Resources/routes/auth.php
+++ src/App/Resources/routes/auth.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Middleware\GuestMiddleware;
+use App\Middleware\AuthMiddleware;
+
 $app->group('', function () {
     $this->map(['GET', 'POST'], '/login', 'AuthController:login')->setName('login');
     $this->map(['GET', 'POST'], '/register', 'AuthController:register')->setName('register');
-})->add(new App\Middleware\GuestMiddleware($container));
+})->add(new GuestMiddleware($container));
 
 $app->group('', function () {
     $this->get('/logout', 'AuthController:logout')->setName('logout');
-})->add(new App\Middleware\AuthMiddleware($container));
+})->add(new AuthMiddleware($container));
