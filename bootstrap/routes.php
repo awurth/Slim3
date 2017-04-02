537d0f46ea5e77c338bd8257eaae150ae3244143
Alexis Wurth <alexis.wurth57@gmail.com>
2017-04-03 00:49:42 +0200

    Use a constant instead of variable

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-$dir = __DIR__ . '/../src/App/Resources/routes/';
+const ROUTES_DIR = __DIR__ . '/../src/App/Resources/routes/';
 
-require $dir . 'app.php';
-require $dir . 'auth.php';
+require ROUTES_DIR . 'app.php';
+require ROUTES_DIR . 'auth.php';
