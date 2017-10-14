4518b92147e29c5e6fb5501d4cac7d615d22aa6c
Alexis Wurth <alexis.wurth57@gmail.com>
2017-10-14 17:17:33 +0200

    Removed unused parameter

--- src/Admin/Resources/config/routing.php
+++ src/Admin/Resources/config/routing.php
@@ -1,6 +1,6 @@
 <?php
 
-$app->group('/admin', function () use ($container) {
+$app->group('/admin', function () {
     $this->get('', 'admin.controller:home')
         ->setName('admin.home');
 })->add($container['auth.middleware']('admin'));
