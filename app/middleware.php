ad54cfbe11d6921eed9bf15ed4cdeebcf2bdda0d
Alexis Wurth <alexis.wurth57@gmail.com>
2017-10-14 15:28:18 +0200

    Registered middlewares in the container

--- app/middleware.php
+++ app/middleware.php
@@ -1,3 +1,16 @@
 <?php
 
+use Security\Middleware\AuthMiddleware;
+use Security\Middleware\GuestMiddleware;
+
+$container['guest.middleware'] = function ($container) {
+    return new GuestMiddleware($container['router'], $container['auth']);
+};
+
+$container['auth.middleware'] = function ($container) {
+    return function ($role = null) use ($container) {
+        return new AuthMiddleware($container['router'], $container['flash'], $container['auth'], $role);
+    };
+};
+
 $app->add($container['csrf']);
--- src/Security/Resources/config/routing.php
+++ src/Security/Resources/config/routing.php
@@ -1,13 +1,10 @@
 <?php
 
-use Security\Middleware\GuestMiddleware;
-use Security\Middleware\AuthMiddleware;
-
 $app->group('', function () {
     $this->map(['GET', 'POST'], '/login', 'auth.controller:login')->setName('login');
     $this->map(['GET', 'POST'], '/register', 'auth.controller:register')->setName('register');
-})->add(new GuestMiddleware($container));
+})->add($container['guest.middleware']);
 
 $app->get('/logout', 'auth.controller:logout')
-    ->add(new AuthMiddleware($container))
+    ->add($container['auth.middleware']())
     ->setName('logout');
