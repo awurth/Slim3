d98873a13cb6f03ad0b23fcc824f219f0ee2c9aa
Alexis Wurth <awurth.dev@gmail.com>
2017-12-09 11:04:18 +0100

    Changed logs dir to var/log/

--- src/Application.php
+++ src/Application.php
@@ -55,7 +55,7 @@ class Application extends App
 
     public function getLogDir()
     {
-        return $this->getRootDir().'/var/logs';
+        return $this->getRootDir().'/var/log';
     }
 
     public function getRootDir()
